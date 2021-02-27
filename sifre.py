ab4c0af79fe9ccb698d964366d1ed3731c30a3d4
mertfsmal <79642632+mertfsmal@users.noreply.github.com>
2021-02-27 23:35:52 +0300

    Şifre ekleme komudunu ekledim

--- sifre.py
+++ sifre.py
@@ -1,14 +1,52 @@
-# şifre değiştirme komudu eklenecek  hangi sitede olduğunu anlayacak kod eklenecek ingilizce olanı çıkarılacak keylogger kontrol yani şifrelerin çalınmasını engellemek için, uninstall eklemek json dosyasının olduğu yeri bulmak, şifreleri daha güzel gösterecek şekilde {} yerine |- gibi
+# json dosyasında şifre olup olmadığını anlama, kaç tane giriş bilgisi kayıtlı söylemek, belirli giriş bilgilerini silmek, setup dosyasının sifre.py girisbilgiler.json yaratmasını falan sağlamak, json dosyasını yaratma,şifre değiştirme komudu eklenecek  hangi sitede olduğunu anlayacak kod eklenecek ingilizce olanı çıkarılacak keylogger kontrol yani şifrelerin çalınmasını engellemek için, uninstall eklemek json dosyasının olduğu yeri bulmak, şifreleri daha güzel gösterecek şekilde {} yerine |- gibi
 from pynput import keyboard
 import pyperclip #pyperclip'i kütüphaneye ekle
 import json
+def yazdirJson(data, filename='girisBilgileri.json'):
+    with open(filename,'w') as f:
+        json.dump(data, f, indent=1)
+with open('girisBilgileri.json') as jsonDosyasi:
+    data = json.load(jsonDosyasi)
+    temp = data['Giris bilgileri']
 girisBilgileri = json.load(open('girisBilgileri.json'))
-girisBilgileriPrint = json.dumps(girisBilgileri, indent=1)
 print('Şifreleri görmek için "sifreler",')
 print('Giriş bilgilerini değiştirmek için "degistir",')
-print('----------------------------------------------')
+print('Şifre eklemek için "ekle",')
+print('Şifreleri sıfırlamak için "sifirla",')
+print('Önceden kaydedilen giriş bilgilerini kopyalamak için site adını girin')
 while True:
+    girisBilgileriPrint = json.dumps(girisBilgileri, indent=1)
+    print('----------------------------------------------')
     cevap=input('') #Cevap nedir
+    if cevap=='sifirla':
+        print('Şifrenizi sıfırlamak istediğinize emin misiniz?(e/h)')
+        cevap=input('')
+        """if cevap=='e':
+            girisBilgileriSifirla = {
+            Giris bilgileri:[{
+            }]}
+            print(girisBilgileriPrint)
+            input('')"""
+    if cevap=='ekle':
+        print('Hangi sitenin giriş bilgisini kaydetmek istersiniz')
+        siteEkle=input('')
+        print('e-posta giriniz')
+        epostaEkle=input('')
+        print('kullanıcı adı giriniz')
+        kullaniciAdiEkle=input('')
+        print('sifre giriniz')
+        sifreEkle=input('')
+        print('Site:',siteEkle, 'e-posta:',epostaEkle, 'Sifre:',sifreEkle, 'eklemek istediğinize emin misiniz?(e/h)')
+        cevap=input('')
+        if cevap=='e':
+            girisBilgileriEkle = {
+            siteEkle:[{
+            "e-posta": epostaEkle,
+            "kullanici adi":kullaniciAdiEkle,
+            "sifre": sifreEkle
+            }]}
+            temp.append(girisBilgileriEkle)
+            yazdirJson(data)
     if cevap=='degistir':
         print(girisBilgileriPrint)
         print('----------------------------------------------')
