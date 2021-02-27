d3ee729a08b5cc793a5064491b190b907fa85080
mertfsmal <79642632+mertfsmal@users.noreply.github.com>
2021-02-27 23:58:10 +0300

    Json dosyasını yaratma ekledim

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+import json
 import pip
 print('Python kütüphanenize Pynput ve Pyperclip ekleniyor..')
 pip.main(['install','pynput', 'pyperclip']) #Asıl komut
-input('') #Programın kapanmasını engellemek için
+print('Eğer şifre kaydettiyseniz bütün şifreleriniz silinecek emin misiniz?(e/h)')
+cevap=input('')
+if cevap=='e':
+    with open('girisBilgileri.json', 'w') as f:
+    print("json dosyası yaratıldı")
+    input('')
